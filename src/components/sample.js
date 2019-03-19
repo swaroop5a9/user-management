e658888e895806b7822e94cd5012828f7b7a92bb
Sai Swaroop <swaroop.anu46@gmail.com>
2019-03-19 10:22:40 +0530

    Grid changes

--- src/components/sample.js
+++ src/components/sample.js
@@ -1,5 +1,13 @@
 import React from "react";
 import data from "../people.json";
+import {
+  // Table,
+  // TableHead,
+  // TableCell,
+  // TableBody,
+  // TableRow,
+  Grid
+} from "@material-ui/core";
 class sample extends React.Component {
   constructor(props) {
     super(props);
@@ -19,14 +27,57 @@ class sample extends React.Component {
   render() {
     return (
       <div>
-        {data.People.map((item, index) => (
-          <li key={index} onClick={() => this.handleClick(item)}>
-            {item.name}
-          </li>
-        ))}
+        <Grid container spacing={8} justify="center">
+          {/* <Grid item xs={3}> */}
+          {/* <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Rating</TableCell>
+                  <TableCell>Description</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody> */}
+          {data.People.map((item1, index) => (
+            // <li
+            //   style={styles.itemStyle}
+            //   key={index}
+            //   onClick={() => this.handleClick(item)}
+            // >
+            //   {item.name}
+            // </li>
+            // <TableRow key={index}>
+            //   <TableCell>{item.name}</TableCell>
+            //   <TableCell>{item.rating}</TableCell>
+            //   <TableCell>{item.Description}</TableCell>
+            // </TableRow>
+            // <Grid container item xs={12} key={index}>
+            <React.Fragment key={index}>
+              <Grid  item xs={4}>
+                {item1.name}
+              </Grid>
+              <Grid item xs={4}>
+                {item1.rating}
+              </Grid>
+              <Grid item xs={4}>
+                {item1.Description}
+              </Grid>
+              </React.Fragment>
+            // </Grid>
+          ))}
+          {/* </TableBody>
+            </Table> */}
+          {/* </Grid> */}
+        </Grid>
       </div>
     );
   }
 }
+
+const styles = {
+  itemStyle: {
+    listStyleType: "none"
+  }
+};
 
 export default sample;
--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Sample from "../components/sample";
 import UserDetails from "../components/UserDetails";
+import { Card, CardContent, Grid, Paper } from "@material-ui/core";
 class SamplePage extends React.Component {
   constructor(props) {
     super(props);
@@ -17,8 +18,24 @@ class SamplePage extends React.Component {
   render() {
     return (
       <div>
-        <Sample handleClick={this.handleClick} />
-        <UserDetails item={this.state.item} />
+        <Card>
+          <CardContent>
+            <Grid item xs={12}>
+              <Grid container justify="center" spacing={8}>
+                <Grid item>
+                  <Paper>
+                    <Sample handleClick={this.handleClick} />
+                  </Paper>
+                </Grid>
+                <Grid item>
+                  <Paper>
+                    <UserDetails item={this.state.item} />
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
       </div>
     );
   }
